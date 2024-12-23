4c455e046331e16be8e91638b628b050a99749cf
Eugen Dirksen <eugen.dirksen@check24.de>
2024-12-23 19:52:38 +0100

    [ED-2] make weight component independent

--- src/01-presentation/pages/balance-scale/components/Weight.tsx
+++ src/01-presentation/pages/balance-scale/components/Weight.tsx
@@ -1,40 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import {
-  selectBalanceScale,
-  useStore,
-} from "../../../../03-data/store/useStore.ts";
 
 type Props = {
-  weight: number;
+  weight: number | null;
   showRemove?: boolean;
+  onAddWeight?: (weight: number) => void;
+  onRemoveWeight?: (weight: number) => void;
+  onError?: (message: string) => void;
 };
-export const Weight: React.FC<Props> = ({ weight, showRemove = false }) => {
-  const { removeWeightFromStock } = useStore(selectBalanceScale);
+export const Weight: React.FC<Props> = ({
+  weight,
+  showRemove = false,
+  onRemoveWeight = () => {},
+  onAddWeight = () => {},
+  onError = () => {},
+}) => {
+  const [newWeight, setNewWeight] = useState<string>("");
 
-  function handleRemove(): void {
-    removeWeightFromStock(weight);
-  }
+  const handleAddWeight = (): void => {
+    const cleanedWeightString: string = newWeight.replace(/[^0-9.]/g, "");
+    const weight: number = Number(cleanedWeightString);
+
+    if (isNaN(weight) || weight <= 0) {
+      onError("Please enter a valid weight");
+      return;
+    }
+    onAddWeight(weight);
+  };
+
+  const handleKeyDown = (
+    event: React.KeyboardEvent<HTMLInputElement>,
+  ): void => {
+    if (event.key === "Enter") {
+      handleAddWeight();
+    }
+  };
+
+  const handleRemoveWeight = (): void => {
+    if (weight !== null) {
+      onRemoveWeight(weight);
+    }
+  };
 
   return (
-    <>
-      <StyledWeight>
-        {showRemove && (
-          <TrashIcon onClick={handleRemove} title="Remove weight">
-            x
-          </TrashIcon>
-        )}
-        {weight}kg
-      </StyledWeight>
-    </>
+    <WeightIcon>
+      {showRemove && (
+        <WeightDeleteIcon onClick={handleRemoveWeight}>x</WeightDeleteIcon>
+      )}
+
+      {weight === null ? (
+        <WeightInput
+          value={newWeight}
+          autoFocus
+          onChange={(event) => setNewWeight(event.target.value)}
+          onBlur={handleAddWeight}
+          onKeyDown={handleKeyDown}
+        />
+      ) : (
+        `${weight}`
+      )}
+      <span>kg</span>
+    </WeightIcon>
   );
 };
 
-const TrashIcon = styled.div`
+const WeightDeleteIcon = styled.div`
   position: absolute;
   top: 0;
   right: 0;
-  font-size: 1.2rem;
+  font-size: 2rem;
+  line-height: 0;
   cursor: pointer;
 
   &:hover {
@@ -42,7 +77,7 @@ const TrashIcon = styled.div`
   }
 `;
 
-const StyledWeight = styled.div`
+const WeightIcon = styled.div`
   position: relative;
   width: 100px;
   height: 100px;
@@ -51,7 +86,21 @@ const StyledWeight = styled.div`
   text-align: center;
   padding-top: 45px;
   background-image: url("../../../../../public/weight.svg");
-  background-size: contain;
-  background-repeat: no-repeat;
   background-position: center;
+`;
+
+const WeightInput = styled.input<{ value: string }>`
+  display: inline-block;
+  width: ${(props) => Math.max(props.value.length, 2)}ch;
+  border: unset;
+  font-size: 1.5rem;
+  font-weight: bolder;
+  text-align: center;
+  outline: 4px solid var(--accent-background-color);
+  border-radius: 3px;
+
+  &:focus {
+    outline: 4px solid var(--accent-background-color);
+    border-radius: 3px;
+  }
 `;
